98900e634d73e747eeadef25055db91817dd3e2f
Suryash-Jha <joblesscoder2468@gmail.com>
2024-11-02 14:12:48 +0530

    Basic llama3.2 model as flask api done

--- app.py
+++ app.py
@@ -1,1 +1,26 @@
 # Ollama connection using python llama3.2
+
+from flask import Flask, jsonify, request 
+from langchain_ollama import OllamaLLM
+
+app = Flask(__name__)
+llm = OllamaLLM(model="llama3.2")
+
+def getAiResponse(userInput):
+    response = llm.invoke(userInput)
+    return response
+
+@app.route('/', methods = ['GET', 'POST']) 
+def home(): 
+    if(request.method == 'GET'): 
+        data = "Suryash's Ollama running! Send POST requests with your query!"
+    if(request.method == 'POST'):
+        userInput= request.json
+        data= getAiResponse(userInput['userInput'])
+    return jsonify({'data': data}) 
+
+
+
+
+if __name__ == '__main__': 
+    app.run(debug = True) 
